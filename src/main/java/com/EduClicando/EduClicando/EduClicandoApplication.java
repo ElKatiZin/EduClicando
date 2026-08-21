db12bba6e5f49ba059817a0e553d97e8904f39e5
ElKatiZin <mister.katp@gmail.com>
2026-08-21 20:47:10 -0300

    Arquivos iniciais do SpringBootInitializer

---
+++ src/main/java/com/EduClicando/EduClicando/EduClicandoApplication.java
@@ -0,0 +1,13 @@
+package com.EduClicando.EduClicando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduClicandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduClicandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EduClicando/EduClicando/EduClicandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EduClicando.EduClicando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduClicandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
